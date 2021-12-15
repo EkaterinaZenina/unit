dd752cbc07b5c34fdb42864ee0d371af76ae44ea
Екатерина Зенина <zeninaekaterin@gmail.com>
2021-12-15 11:07:29 +0300

    init

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTestNG {
+
+    @Test
+    public void shouldCalculateCashbackFrom1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashbackFrom1100() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCalculateCashbackFrom2000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+}
