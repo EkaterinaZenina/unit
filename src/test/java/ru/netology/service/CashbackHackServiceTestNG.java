27446e7579eb07d98c60b9915b27f06a23b246ef
EkaterinaZenina <90308326+EkaterinaZenina@users.noreply.github.com>
2022-01-26 09:21:51 +0300

    Update CashbackHackServiceTestNG.java

--- src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -10,7 +10,7 @@ public class CashbackHackServiceTestNG {
     public void shouldCalculateCashbackFrom1000() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 
